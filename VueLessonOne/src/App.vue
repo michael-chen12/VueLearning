a28e785616813759c19bd0789eec6faa9462bcf7
michael <chen.michael40@gmail.com>
2026-02-17 13:31:10 +0800

    Finished Vue.js LessonOne

--- VueLessonOne/src/App.vue
+++ VueLessonOne/src/App.vue
@@ -1,11 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+interface State {
+  name: string
+  age: number
+  isStudent: boolean
+}
+const state = reactive<State>({
+  name: 'Michael',
+  age: 17,
+  isStudent: false,
+})
+const isShow = ref<boolean>(true)
+const hobbies = reactive<string[]>(['Reading', 'Traveling', 'Cooking'])
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <h1>Welcome, {{ state.name }}!</h1>
+    <p v-show="isShow">Age: {{ state.age }}</p>
+    <p v-show="isShow">Status: {{ state.isStudent ? 'Student' : 'Not a Student' }}</p>
+  </div>
+  <div>
+    <button @click="state.age++">Increase Age</button>
+    <button @click="state.isStudent = !state.isStudent">Toggle Student Status</button>
+    <button @click="isShow = !isShow">{{ isShow ? 'Hide' : 'Show' }}</button>
+  </div>
+  <ol>
+    <li v-for="hobby in hobbies" :key="hobby">{{ hobby }}</li>
+  </ol>
+  <section v-if="state.age >= 18">
+    <h2>Adult Content</h2>
+    <p>This content is only visible to users who are 18 years or older.</p>
+  </section>
 </template>
-
-<style scoped></style>
